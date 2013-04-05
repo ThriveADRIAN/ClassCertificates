96e125443c9ab36e7c38bd555fd743a6fcedbe20
Adrian Cunanan <adrian.cunanan@me.com>
2013-04-05 16:06:39 -0400

    Exercise Completed

--- ClassCertificates/main.c
+++ ClassCertificates/main.c
@@ -7,12 +7,24 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+void congratulateStudent ( char *student, char *course, int numDays )
+{
+    printf("%s has done as much %s Programming as I could fit into %d days.\n", student , course, numDays);
+}
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    congratulateStudent("Mark", "Cocoa", 5);
+    sleep(2);
+    congratulateStudent("Bo", "Objective-C", 2);
+    sleep(2);
+    congratulateStudent("Mike", "Python", 5);
+    sleep(2);
+    congratulateStudent("Ted", "iOS", 5);
+    sleep(2);
     return 0;
 }
 
